8017aac18041991fde2350999084e13be68e1ffe
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2024-02-29 15:44:19 +0100

    Prilagodjena funkcija register za FrontEnd

--- database/migrations/2024_01_19_213903_create_auctions_table.php
+++ database/migrations/2024_01_19_213903_create_auctions_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->decimal('start_price', 10, 2)->nullable();
             $table->decimal('current_price', 10, 2)->default(DB::raw('start_price'));
             $table->timestamp('start')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('end')->default(DB::raw('DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 2 MINUTE)'));
+            $table->timestamp('end')->default(DB::raw('DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 2 DAY)'));
             $table->string('image_path')->nullable();
             $table->timestamps();
         });
