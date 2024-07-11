79a2102c58b11ef1b484c69ecacc9a48b0e9fc42
Ivan Jelisavcic <ivanjelisavcic6@gmail.com>
2024-07-11 16:53:40 +0200

    Dodati Jobs-ovi za izvrsavanje bidovanja na aukcije

--- app/Http/Controllers/BidController.php
+++ app/Http/Controllers/BidController.php
@@ -13,40 +13,42 @@ class BidController extends Controller
 {
     public function placeBid(Request $request)
     {
-        $validator=Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'auction_id' => 'required|exists:auctions,id'
         ]);
-        if($validator->fails()){
+
+        if ($validator->fails()) {
             return response()->json($validator->errors());
         }
 
+
         $auctionId = $request->auction_id;
         $auction = Auction::find($auctionId);
-
         $user = Auth::user();
-        $userBefore = User::find($auction->current_bidder);
+        if ($auction->is_locked || $user->is_locked) {
+            return response()->json(['message' => 'Auction is locked.'], 401);
+        }
+        $auction = Auction::lockForUpdate()->find($auctionId);
+        $user = User::lockForUpdate()->find($user->id);
         if (!$user) {
             return response()->json(['message' => 'You have to be logged in to make a bid.'], 401);
         }
+
         if ($auction->user_id === $user->id) {
             return response()->json(['message' => 'You cannot bid on your own auction.'], 400);
         }
+
         if ($auction->current_bidder === $user->id) {
             return response()->json(['message' => 'You are already the highest bidder.'], 400);
         }
+
         $bidAmount = max($auction->current_price, $auction->start_price);
         if ($user->balance < $bidAmount) {
             return response()->json(['message' => 'You don\'t have enough funds in your account'], 400);
         }
-        if($userBefore){
-            $userBefore->balance+=($bidAmount/1.05);
-            $userBefore->save();
-        }
-        $auction->current_price=$bidAmount*1.05;
-        $auction->current_bidder=$user->id;
-        $auction->save();
-        $user->balance-=$bidAmount;
-        $user->save();
+
+        // Dispatch job
+        ProcessBid::dispatch($auctionId, $user->id, $bidAmount);
 
 
         return response()->json(['message' => 'Bid made successfully','new_balance'=>$user->balance]);
